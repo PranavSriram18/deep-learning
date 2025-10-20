1fdba38b64aab463540e1414e440a15c3d8dc736
Pranav Sriram <pranav.a.sriram@gmail.com>
2025-10-20 19:24:52 +0530

    add data dir

---
+++ data/base_loader.py
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+from enum import Enum, auto 
+from typing import Optional
+
+import torch  # type: ignore
+
+class DataMode(Enum):
+    TRAIN = auto()
+    EVAL = auto()
+    TEST = auto()
+
+class BaseLoader:
+    def __init__(
+        self, batch_size: int, tokens_per_batch: int, vocab_size: Optional[int]
+    ):
+        self._batch_size = batch_size
+        self._tokens_per_batch = tokens_per_batch
+        self._vocab_size = vocab_size
+
+    def vocab_size(self) -> int:
+        """Returns the effective vocab size for this data loader."""
+        return self.vocab_size
+
+    @abstractmethod
+    def get_batch(self, mode: DataMode) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Returns a tuple x, y of data.
+        Child classes should document shapes of x, y.
+        """
+        pass
+
+    @abstractmethod
+    def encode(self, tokens: list[str]) -> list[int]:
+        """
+        Encodes a list of tokens into a list of IDs.
+        """
+        pass
+
+    @abstractmethod
+    def decode(self, token_ids: list[int]) -> list[str]:
+        """
+        Decodes a list of IDs into a list of tokens.
+        """
+        pass
